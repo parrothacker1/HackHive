a3afa8e39080467e31fee81de2980c88b19e8a5e
parrothacker1 <rajeevgoutham1@protonmail.com>
2025-01-28 02:41:36 +0530

    feat:Added config support

--- services/users/database/db.go
+++ services/users/database/db.go
@@ -1,10 +1,7 @@
 package database
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-
+	"github.com/parrothacker1/Solvelt/users/config"
 	"github.com/parrothacker1/Solvelt/users/models"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
@@ -13,40 +10,8 @@ import (
 
 var DB *gorm.DB
 
-func ConnnectToDatabase() {
-  port_str := os.Getenv("DB_PORT")
-  if port_str == "" {
-    logrus.Fatal("The DB_PORT is not specified")
-  }
-  port,err := strconv.ParseUint(port_str,10,32)
-  if err != nil {
-    logrus.Fatal("There was an error in parsing DB_PORT: %v\n",err)
-  }
-  host := os.Getenv("DB_HOST")
-  if host == "" {
-    logrus.Fatal("The DB_HOST is not specified")
-  }
-  user := os.Getenv("DB_USER")
-  if  user == "" {
-    logrus.Fatal("The DB_USER is not specified")
-  }
-  password := os.Getenv("DB_PASSWORD")
-  if password == "" {
-    logrus.Fatal("The DB_PASSWORD is not specified")
-  }
-  db := os.Getenv("DB_DATABASE")
-  if db == "" {
-    logrus.Fatal("The DB_DATABASE is not specified")
-  }
-  connection_str := fmt.Sprintf(
-    "host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-    host,
-    port,
-    user,
-    password,
-    db,
-  )
-  DB, err = gorm.Open(postgres.Open(connection_str), &gorm.Config{
+func ConnnectToDatabase() { 
+  DB, err := gorm.Open(postgres.Open(config.ConnectionString), &gorm.Config{
     SkipDefaultTransaction: true,
     TranslateError: true,
   })
