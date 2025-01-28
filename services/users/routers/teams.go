2ebcd94cd8e6e87ef3e0e125dbe9990f02870f0d
parrothacker1 <rajeevgoutham1@protonmail.com>
2025-01-28 19:03:17 +0530

    chore:Completed routers

--- services/users/routers/teams.go
+++ services/users/routers/teams.go
@@ -1,1 +1,19 @@
 package routers
+
+import (
+	"net/http"
+
+	"github.com/parrothacker1/Solvelt/users/handlers"
+	"github.com/parrothacker1/Solvelt/users/middlewares"
+)
+
+var TeamRouter *Router
+
+func init() {
+  TeamRouter = NewRouter()
+  TeamRouter.Handle(http.MethodPost,"/",handlers.CreateTeam,middlewares.AuthMiddleware)
+  TeamRouter.Handle(http.MethodPut,"/",handlers.UpdateTeam,middlewares.AuthMiddleware)
+  TeamRouter.Handle(http.MethodDelete,"/",handlers.DeleteTeam,middlewares.AuthMiddleware)
+  TeamRouter.Handle(http.MethodPost,"/",handlers.JoinTeam,middlewares.AuthMiddleware)
+  TeamRouter.Handle(http.MethodGet,"/me",handlers.GetTeam,middlewares.AuthMiddleware)
+}
--- services/users/routers/users.go
+++ services/users/routers/users.go
@@ -1,1 +1,20 @@
 package routers
+
+import (
+	"net/http"
+
+	"github.com/parrothacker1/Solvelt/users/handlers"
+	"github.com/parrothacker1/Solvelt/users/middlewares"
+)
+
+var UserRouter *Router 
+
+func init() {
+  UserRouter = NewRouter()
+  UserRouter.Handle(http.MethodPost,"/",handlers.CreateUser)
+  UserRouter.Handle(http.MethodPut,"/",handlers.UpdateUser,middlewares.AuthMiddleware)
+  UserRouter.Handle(http.MethodGet,"/me",handlers.GetUser,middlewares.AuthMiddleware)
+  UserRouter.Handle(http.MethodDelete,"/",handlers.DeleteUser,middlewares.AuthMiddleware)
+  UserRouter.Handle(http.MethodPost,"/login",handlers.LoginUser)
+  UserRouter.Handle(http.MethodPost,"/reset",handlers.ResetPassword,middlewares.AuthMiddleware)
+}
